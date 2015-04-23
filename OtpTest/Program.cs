5e3d784e60e35ca864b89e55d5082516a3a7e072
variel <blackmagic2@naver.com>
2015-04-23 21:52:34 +0900

    First create

---
+++ OtpTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Variel.Security;
+
+namespace Variel.Security.Otp.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Otp otp = new Otp("AASZQOWDXO");
+            while(true)
+                Console.WriteLine((string) otp.GetPassword());
+        }
+    }
+}
